224218a77f3188af31d040e9cfc6227d6c555bcc
Tidiane <tidianecs20pgmail.com>
2025-12-12 08:53:25 +0000

    Done login/sign up + var environment

--- projects_manager/src/app/app-routing.module.ts
+++ projects_manager/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './layouts/auth/register/register.component';
+import { LoginComponent } from './layouts/auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
